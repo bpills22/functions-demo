0cefec3a65288f17bcab01fdad1dd9da20b7bd7e
Brian <pillsbury_b@yahoo.com>
2023-09-20 16:03:56 -0400

    static hosting edgio

---
+++ routes.js
@@ -0,0 +1,9 @@
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+import { Router } from '@edgio/core/router'
+import { nodejsRoutes } from '@edgio/nodejs-connector'
+
+export default new Router()
+  // automatically adds all routes from the Node.js connector
+  .get('/:path*', ({ serveStatic }) => serveStatic('src/hw1.html'))
+  //.use(nodejsRoutes)
